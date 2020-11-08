195c99ddd3d200ca47c9ef8a24734960d9efeb6d
emircankilinc <emircan_kilinc@hotmail.com>
2020-11-08 17:11:23 +0300

    properties  update

--- takeaway_game/src/main/java/com/takeaway/takeaway_game/TakeawayGameApplication.java
+++ takeaway_game/src/main/java/com/takeaway/takeaway_game/TakeawayGameApplication.java
@@ -4,7 +4,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.scheduling.annotation.EnableScheduling;
 
 import com.takeaway.takeaway_game.server.RabbitMqExample;
 
