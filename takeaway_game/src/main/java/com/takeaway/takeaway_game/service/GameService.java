9b42264703ee43578631332f5ee4783a2b7e14d3
emircankilinc <emircan_kilinc@hotmail.com>
2020-11-09 22:16:17 +0300

    start service integration

--- takeaway_game/src/main/java/com/takeaway/takeaway_game/service/GameService.java
+++ takeaway_game/src/main/java/com/takeaway/takeaway_game/service/GameService.java
@@ -5,13 +5,15 @@ import java.util.Map;
 
 import javax.websocket.server.PathParam;
 
+import org.springframework.amqp.AmqpException;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.takeaway.takeaway_game.entity.Player;
 import com.takeaway.takeaway_game.server.GameServerProducer;
@@ -27,7 +29,7 @@ public class GameService implements IGameService {
 	@Autowired
 	private RabbitTemplate rabbittemplate;
 
-	private static ObjectMapper obj;
+	private static ObjectMapper obj = new ObjectMapper();
 
 	@GetMapping("/register")
 	@Override
@@ -48,14 +50,13 @@ public class GameService implements IGameService {
 		return clonedMap;
 	}
 
-	@GetMapping("/start")
+	@PostMapping(value = "/start", consumes = "application/json", produces = "application/json")
 	@Override
-	public Boolean startGame(@PathParam("player") String player) {
+	public Boolean startGame(@RequestBody Player player) {
 		try {
-			Player readValue = new ObjectMapper().readValue(player, Player.class);
-			String writeValueAsString = obj.writeValueAsString(readValue);
-			rabbittemplate.convertAndSend("x.game", readValue.getRivalId().toString(), writeValueAsString);
-		} catch (JsonMappingException e) {
+			rabbittemplate.convertAndSend("x.game", player.getRivalId().toString(),
+					new ObjectMapper().writeValueAsString(player));
+		} catch (AmqpException e) {
 			return Boolean.FALSE;
 		} catch (JsonProcessingException e) {
 			return Boolean.FALSE;
