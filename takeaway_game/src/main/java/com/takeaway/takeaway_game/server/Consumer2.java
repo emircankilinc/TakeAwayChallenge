932f7c93a49df4750e2464824571e68bd524da09
emircankilinc <emircan_kilinc@hotmail.com>
2020-11-09 11:24:00 +0300

    consumer 2 added useless

--- takeaway_game/src/main/java/com/takeaway/takeaway_game/server/Consumer2.java
+++ takeaway_game/src/main/java/com/takeaway/takeaway_game/server/Consumer2.java
@@ -6,9 +6,9 @@ import org.springframework.stereotype.Service;
 @Service
 public class Consumer2 {
 
-	@RabbitListener(queues = "q.game.1")
-	private void consume(String deneme) {
-		System.out.println(deneme + this.getClass().toString());
-	}
+//	@RabbitListener(queues = "q.game.1")
+//	private void consume(String deneme) {
+//		System.out.println(deneme + this.getClass().toString());
+//	}
 
 }
